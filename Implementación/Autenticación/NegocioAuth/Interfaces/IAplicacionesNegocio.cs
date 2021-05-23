b83f4483407e0300d45bfdc31cc53fd1ded075af
Michael Andres Vargas Buitrago <mavargasb@husi.org.co>
2021-05-23 10:08:14 -0500

    Remove Project

--- Implementación/Autenticación/NegocioAuth/Interfaces/IAplicacionesNegocio.cs
+++
@@ -1,9 +0,0 @@
-﻿using DominioAuth.Modelo;
-
-namespace NegocioAuth.Interfaces
-{
-    public interface IAplicacionesNegocio : INegocioBase<Aplicacion>
-    {
-        Configuracion verConfiguracion(string IdAplicacion);
-    }
-}
