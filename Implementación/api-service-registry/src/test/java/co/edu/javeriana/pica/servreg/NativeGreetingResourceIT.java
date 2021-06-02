76a2c55b5a6e5980b303721bcc8a4378c6206612
Mauricio Fernando Ortiz <mauriort@gmail.com>
2021-06-02 18:10:26 -0500

    cambio de version

--- Implementación/api-service-registry/src/test/java/co/edu/javeriana/pica/servreg/NativeGreetingResourceIT.java
+++
@@ -1,9 +0,0 @@
-package co.edu.javeriana.pica.servreg;
-
-import io.quarkus.test.junit.NativeImageTest;
-
-@NativeImageTest
-public class NativeGreetingResourceIT extends GreetingResourceTest {
-
-    // Execute the same tests but in native mode.
-}
--- Implementación/api-service-registry/startup.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-sudo docker build . -t javeriana-aes-pica/challengers/api-service-registry-img:1.0.0
-sudo docker run -d -p 9000:9000 -p 3306:3306 --name api-service-registry javeriana-aes-pica/challengers/api-service-registry-img:1.0.0
