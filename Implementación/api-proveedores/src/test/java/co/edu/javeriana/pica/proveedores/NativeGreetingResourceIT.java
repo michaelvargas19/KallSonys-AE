9358477bcef1ae527f4e6f7ee45f9f6d2ca1cbe5
Mauricio Fernando Ortiz <mauriort@gmail.com>
2021-06-02 06:59:20 -0500

    Subir nuevos fuentes

--- Implementación/api-proveedores/src/test/java/co/edu/javeriana/pica/proveedores/NativeGreetingResourceIT.java
+++
@@ -1,9 +0,0 @@
-package co.edu.javeriana.pica.proveedores;
-
-import io.quarkus.test.junit.NativeImageTest;
-
-@NativeImageTest
-public class NativeGreetingResourceIT extends GreetingResourceTest {
-
-    // Execute the same tests but in native mode.
-}
